5f5a920c0bffd1b01c45273a2a00b8cc0918ac57
Rdern <467379854@qq.com>
2024-09-03 20:56:28 +0800

    Add files via upload

---
+++ qwq/aaa.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>rd与tzr</title>
+<h1>tzr呆，rd美</h1>
+<style type="text/css">
+h1{color:red};
+p{color:red};
+</style>
+</head>
+
+<?php
+	echo'谭卓然是傻狗';
+	echo"<p>但是他很可爱</p>";
+	echo"<h2>所以rd很喜欢他</h2>";
+	
+	
+
+?>
+</body>
+</html>
